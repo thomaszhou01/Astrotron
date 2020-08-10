fefb67e1217bb8b6bf8ef27ca5f0e24f1e27fb45
Thomas Zhou <tomahawktothedome@gmail.com>
2020-08-09 23:33:06 -0400

    08/09

--- game1/Scenes/Misc/nextLevelZone.gd
+++ game1/Scenes/Misc/nextLevelZone.gd
@@ -21,22 +21,43 @@ func _on_nextLevelZone_body_entered(body):
 		var map = load("res://Scenes/Menu.tscn").instance()
 		if Global.onLevel < nextLevel:
 			Global.onLevel += 1
-		if Global.gunID != null:
-			Global.globalGunIDs.append(Global.gunID.filename)
-			Global.globalGunAmmos.append(Global.gunID.ammo)
-			Global.globalGunClips.append(Global.gunID.clipAmmo)
+			#first time add guns
+			if Global.gunID != null:
+				Global.globalGunIDs.append(Global.gunID.filename)
+				Global.globalGunAmmos.append(Global.gunID.ammo)
+				Global.globalGunClips.append(Global.gunID.clipAmmo)
+			else:
+				Global.globalGunIDs.append(null)
+				Global.globalGunAmmos.append(0)
+				Global.globalGunClips.append(0)
+			
+			if Global.gunID2 != null:
+				Global.globalGunIDs.append(Global.gunID2.filename)
+				Global.globalGunAmmos.append(Global.gunID2.ammo)
+				Global.globalGunClips.append(Global.gunID2.clipAmmo)
+			else:
+				Global.globalGunIDs.append(null)
+				Global.globalGunAmmos.append(0)
+				Global.globalGunClips.append(0)
 		else:
-			Global.globalGunIDs.append(null)
-			Global.globalGunAmmos.append(0)
-			Global.globalGunClips.append(0)
-		if Global.gunID2 != null:
-			Global.globalGunIDs.append(Global.gunID2.filename)
-			Global.globalGunAmmos.append(Global.gunID2.ammo)
-			Global.globalGunClips.append(Global.gunID2.clipAmmo)
-		else:
-			Global.globalGunIDs.append(null)
-			Global.globalGunAmmos.append(0)
-			Global.globalGunClips.append(0)
+			#repeated change guns
+			if Global.gunID != null:
+				Global.globalGunIDs[gunIndex-1] = (Global.gunID.filename)
+				Global.globalGunAmmos[gunIndex-1] = (Global.gunID.ammo)
+				Global.globalGunClips[gunIndex-1] = (Global.gunID.clipAmmo)
+			else:
+				Global.globalGunIDs[gunIndex-1] = (null)
+				Global.globalGunAmmos[gunIndex-1] = (0)
+				Global.globalGunClips[gunIndex-1] = (0)
+			
+			if Global.gunID2 != null:
+				Global.globalGunIDs[gunIndex] = (Global.gunID2.filename)
+				Global.globalGunAmmos[gunIndex] = (Global.gunID2.ammo)
+				Global.globalGunClips[gunIndex] = (Global.gunID2.clipAmmo)
+			else:
+				Global.globalGunIDs[gunIndex] = (null)
+				Global.globalGunAmmos[gunIndex] = (0)
+				Global.globalGunClips[gunIndex] = (0)
 		SceneTransition.fadeIn()
 		yield(get_tree().create_timer(1), "timeout")
 		get_parent().get_parent().queue_free()
